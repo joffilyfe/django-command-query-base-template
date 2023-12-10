940ed1af227b088ccc0003d61b15933e74eef540
Joffily Ferreira <joffilyferreira@gmail.com>
2023-12-10 16:28:27 -0300

    Adds QueryObject base class and decorator

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 from typing import Optional, Iterable
 
+
 class BaseAuthorizer:
     """BaseAuthorizer defines how child classes should behavior
     when called by EntryPoints"""
@@ -11,7 +12,7 @@ class BaseAuthorizer:
         raise NotImplementedError
 
 
-class BaseAction():
+class BaseAction:
     """BaseAction class defines how action classes behave"""
 
     def __call__(self):
@@ -43,10 +44,34 @@ class BaseEntryPoint:
         for authorizer in self.authorizers:
             authorizer(self).check()
 
+
 def entry_point(cls):
     """Decorates entry points classes"""
 
     class Base(cls, BaseEntryPoint):
+        ...
+
+    return Base
+
+
+class Meta(type):
+    def __getattribute__(cls, name: str):
+        if name == "objects" and cls.MODEL:
+            return getattr(cls.MODEL, name)
+
+        return super().__getattribute__(name)
+
+
+class BaseQuery(metaclass=Meta):
+    @classmethod
+    def by_pk(cls, pk: int):
+        return cls.MODEL.objects.get(pk=pk)
+
+
+def query(cls):
+    """Alternative decorator that injects BaseQuery Meta Class"""
+
+    class Base(cls, BaseQuery):
         ...
 
     return Base
